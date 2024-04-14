9bf475cb04552cc013b3d9cc2c9b9f4829b86790
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-04-14 14:52:16 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='snake-game',
+    version='0.1',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A classic snake game written in Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/smitwaman/snake-game-python',
+    packages=find_packages(),
+    install_requires=[
+        # Add your requirements here
+        'pygame>=2.0.1',
+    ],
+    entry_points={
+        'console_scripts': [
+            'snake-game = snake-game-python.main:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
